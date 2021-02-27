fdacdb23b15bfadbfdb342399ddbf1a7f60da6c2
Elena Vlasenko <olefelena@gmail.com>
2021-02-27 13:37:14 +0200

    boxes with random rgba-color

--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,7 @@
-const controlContainerEl = document.querySelector('#controls');
+const boxesContainerEl = document.querySelector('#boxes');
 const inputNumberOfBoxesEl = document.querySelector('[type = "number"]');
 const makeBoxesEl = document.querySelector('[data-action="render"]');
 const removeBoxesEl = document.querySelector('[data-action="destroy"]');
-
-// console.log(controlContainerEl);
-// console.dir(inputNumberOfBoxesEl);
-// console.dir(inputNumberOfBoxesEl.value);
-// console.log(makeBoxesEl);
-// console.log(removeBoxesEl);
 
 
 inputNumberOfBoxesEl.addEventListener('input', handleInputChange)
@@ -17,16 +11,6 @@ removeBoxesEl.addEventListener('click', destroyBoxes)
 let boxes = [];
 let amountBoxes = 0;
 
-// for (let i = 0; i <= amountBoxes; i += 1) {
-// const boxEl = document.createElement('div');
-// boxEl.style.backgroundColor = 'tomato'; // как сделать случайный цвет фона??
-//     boxEl.style.width = 30 + i * 10 + 'px';
-// boxEl.style.height = 30 + i * 10 + 'px';
-// boxEl.classList.add('box');
-//     console.log(boxEl);
-//     boxes.push(boxEl);
-// }
-// console.log(boxes);
 
 function handleInputChange(event) {
     amountBoxes = +event.currentTarget.value;
@@ -37,17 +21,18 @@ function createBoxes(amount) {
     amount = amountBoxes;
     for (let i = 0; i <= amountBoxes; i += 1) {
 const boxEl = document.createElement('div');
-boxEl.style.backgroundColor = 'tomato'; // как сделать случайный цвет фона??
+boxEl.style.backgroundColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+
     boxEl.style.width = 30 + i * 10 + 'px';
 boxEl.style.height = 30 + i * 10 + 'px';
 boxEl.classList.add('box');
     console.log(boxEl);
     boxes.push(boxEl);
 }
-controlContainerEl.append(...boxes);
+boxesContainerEl.append(...boxes);
 
 };
 
 function destroyBoxes() {
-    boxes = [];
+    boxesContainerEl.innerHTML = '';
 }
